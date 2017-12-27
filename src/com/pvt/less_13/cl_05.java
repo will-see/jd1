575b48926b65be0b9a315afb8f4a1d3173be22fe
will-see <petrov.dmi3@gmail.com>
2017-12-27 07:25:48 +0300

    lesson 13 done

---
+++ src/com/pvt/less_13/cl_05.java
@@ -0,0 +1,17 @@
+package com.pvt.less_13;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by W510 on 27.12.2017.
+ * Подсчитать количество слов в тексте. Учесть, что слова могут разделяться несколькими пробелами.
+ */
+public class cl_05 {
+    public static void main(String[] args) {
+        String  text = "aa b.    cccc  d e!f?";
+        ArrayList<String> list = new ArrayList<>(Arrays.asList(text.split("[\\s,.!?]+")));
+        System.out.println(list.size());
+        System.out.println(list);
+    }
+}
--- src/com/pvt/less_13/cl_06.java
+++ src/com/pvt/less_13/cl_06.java
@@ -11,16 +11,12 @@ public class cl_06 {
     public static void main(String[] args) {
         String text = "aaabbcdeef";
         StringBuilder sb = new StringBuilder();
+        sb.append(text.substring(0,1));
         for(int i=1; i<text.length();i++){
-//            System.out.print(text.substring(i-1,i));
-//            System.out.print(text.substring(i,i+1));
-
             if (!text.substring(i-1,i).equals(text.substring(i,i+1))){
-//                System.out.println(" false");
-                sb.append(text.substring(i-1,i));
+                sb.append(text.substring(i,i+1));
             }
         }
-//        text= text.replace("\\1{1,}", "$1");
         System.out.println(sb);
     }
 }
