f259583ee2e36beaf6ee182e52c774417fd93070
will-see <petrov.dmi3@gmail.com>
2018-01-03 07:49:22 +0300

    update

--- src/com/pvt/less_09/cl_05/TestTransaction.java
+++ src/com/pvt/less_09/cl_05/TestTransaction.java
@@ -3,20 +3,25 @@ package com.pvt.less_09.cl_05;
 /**
  * Created by W510 on 18.12.2017.
  */
-public class TestTransaction {
+public class    TestTransaction {
 
     public void start() {
         System.out.println("Transaction is started");
         doSmth();
+        doSmth2();
         stop();
     }
-
+//@Transaction
     private void stop() {
         System.out.println("Transaction is stoped");
     }
-
     @Transaction
     private void doSmth() {
         TrasactionAnalyzer.analyze(this.getClass());
     }
+//@Transaction
+    private void doSmth2() {
+        TrasactionAnalyzer.analyze(this.getClass());
+    }
+
 }
