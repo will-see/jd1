5e61de02ea7a157558f5ed76b79fbd1524b87a80
will-see <petrov.dmi3@gmail.com>
2018-01-17 06:50:00 +0300

    less 20

---
+++ src/com/pvt/less_20/Cl_01.java
@@ -0,0 +1,39 @@
+package com.pvt.less_20;
+
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by W510 on 15.01.2018.
+ */
+public class Cl_01 {
+    private static volatile int id = 1;
+
+    static volatile boolean enable = true;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        ExecutorService service = Executors.newSingleThreadExecutor();
+        Scanner sc = new Scanner(System.in);
+            System.out.println("введите число");
+        while (enable) {
+            int timeToSleap=sc.nextInt();
+            if (timeToSleap!=-1) {
+                service.submit(new Runnable() {
+                    public void run() {
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println(Thread.currentThread().getName() + " спал " + timeToSleap );
+                        System.out.println("введите число");
+                    }
+                });
+            }else enable=false;
+        }
+        service.shutdown();
+    }
+}
